ea2110a4573fb186bb4c1b6f37450429ac3ee73d
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-07-22 02:10:19 -0500

    change default port to 4000 since react default is 3000

--- startup/server.js
+++ startup/server.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const app = require('./app');
 
 module.exports = () => {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   const server = app.listen(port);
 
   winston.info(`Listening on port ${port}...`);
