874bc481a9b822bb9e75ba07d9c5e9a42c294828
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-07-26 02:49:28 -0500

    fix send middleware test description

--- tests/unit/middleware/send.test.js
+++ tests/unit/middleware/send.test.js
@@ -1,7 +1,7 @@
 const send = require('../../../src/middleware/send');
 
 describe('send middleware', () => {
-  it('should call next if document is found', async () => {
+  it('should send the requested document if it is found', async () => {
     const req = { doc: 'some document' };
     const res = { send: jest.fn().mockReturnValue(req.doc) };
     await send(req, res);
